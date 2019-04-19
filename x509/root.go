a93ddbbbbb39db6b59e812887646cf0e95b66b11
Andrey Pavlushev <andrey.pavlushev@insolar.io>
2019-04-19 20:20:00 +0300

    Changed "internal/cpu" to "golang.org/x/sys/cpu"

--- x509/root.go
+++ x509/root.go
@@ -11,15 +11,3 @@ var (
 	systemRoots    *CertPool
 	systemRootsErr error
 )
-
-func systemRootsPool() *CertPool {
-	once.Do(initSystemRoots)
-	return systemRoots
-}
-
-func initSystemRoots() {
-	systemRoots, systemRootsErr = loadSystemRoots()
-	if systemRootsErr != nil {
-		systemRoots = nil
-	}
-}
